51b8c092b2dde5bfd7e3a7a0d14a69fe81d05857
jamonda <park00seungjin@gmail.com>
2026-03-14 22:45:47 +0900

    [Gold IV] Title: 중복 제거, Time: 1252 ms, Memory: 328216 KB -BaekjoonHub

--- 백준/Gold/13701. 중복 제거/중복 제거.java
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 /*
  * N개의 정수를 읽고, 반복되는 수를 제외한 남은 수를 순서대로 출력하시오
@@ -14,12 +13,20 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		HashSet<Integer> hs = new HashSet<>();
+		// 2^25까지 주어지지만, int는 32비트 즉 2^5
+		// 그래서 2^25 / 2^5인 2^20을 배정해주어야 한다.
+		int[] bitSet = new int[1 << 20];
+		
 		int size = st.countTokens();
 		for(int i = 0; i < size; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-			if(hs.add(temp)) {
-				bw.write(temp + " ");
+			int X = Integer.parseInt(st.nextToken());
+			
+			int idx = X >> 5; // X / 32와 같다.
+			int rest = X & 31;// X % 31과 같다.
+			
+			if((bitSet[idx] & (1 << rest)) == 0) { // 만약 bitSet[idx] == 000인데 rest가 2(010)이면 & 연산 해서 0이 반환된다.
+				bw.write(X + " ");
+				bitSet[idx] |= (1 << rest); // 000 | 010을 |연산한 010이 반환된다.
 			}
 		}
 		
