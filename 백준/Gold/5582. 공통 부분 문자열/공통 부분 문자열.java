c7ffa5783c2562336b3ec321ce10a4506da4a3f9
jamonda <park00seungjin@gmail.com>
2026-04-11 22:53:04 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 240 ms, Memory: 80252 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+ * 두 개의 문자열이 주어졌을 때, 해당 두 문자열에서 가장 길게 이어지는 문자열의 길이는?
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        char[] A = (" " + str1).toCharArray(); // 문자열을 배열로 변경
+        char[] B = (" " + str2).toCharArray();
+
+        int[][] dp = new int[A.length][B.length];
+
+        int result = 0;
+        for(int i = 1; i < A.length; i++) {
+            for(int j = 1; j < B.length; j++) {
+                if(A[i] == B[j]) {
+                    dp[i][j] = dp[i-1][j-1] + 1; // 문자열이 이어지게 되면 직전의 길이 +1
+                    result = Math.max(result, dp[i][j]); // 그리고 최댓값 비교
+                } else dp[i][j] = 0; // 만약 문자열이 이어지지 않으면 0
+            }
+        }
+
+        System.out.println(result);
+    }
+}
