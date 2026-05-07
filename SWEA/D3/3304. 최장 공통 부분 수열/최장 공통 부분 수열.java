5a626931f2907fc6c4198457ee24518b6f577331
jamonda <park00seungjin@gmail.com>
2026-05-07 21:43:44 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 122 ms, Memory: 32,760 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	
-	static char[] A = new char[1001];
+	static char[] A = new char[1001]; // 넉넉하게 초기화
 	static char[] B = new char[1001];
 	static int[][] dp = new int[1001][1001];
 	
@@ -18,24 +18,23 @@ public class Solution {
 		for(int tc = 1; tc <= TC; tc++) {
 			
 			st = new StringTokenizer(br.readLine());
-			String input1 = st.nextToken();
-			String input2 = st.nextToken();
+			String s1 = st.nextToken(); // 첫 번째 문자열
+			String s2 = st.nextToken(); // 두 번째 문자열
 			
-			int s1 = input1.length();
-			int s2 = input2.length();
+			s1.getChars(0, s1.length(), A, 1); // 문자열의 0번부터 끝까지, A 배열의 [1]번부터 채운다.
+			s2.getChars(0, s2.length(), B, 1);
 			
-			input1.getChars(0, s1, A, 1); // 단일 객체 유지한 상태로 배열에 넣기
-			input2.getChars(0, s2, B, 1);
-			
-			for(int i = 1; i <= s1; i++) {
-				for(int j = 1; j <= s2; j++) {
-					if(A[i] == B[j]) {
-						dp[i][j] = dp[i-1][j-1] + 1;
+			for(int i = 1; i <= s1.length(); i++) {
+				for(int j = 1; j <= s2.length(); j++) {
+					if(A[i] == B[j]) { // 두 문자가 같으면?
+						dp[i][j] = dp[i-1][j-1] + 1; // 직전의 값에서 +1 해주기
+						
+						// 만약 다르면 이전의 위치에서 더 큰 값을 끌어온다.
 					} else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 				}
 			}
 			
-			sb.append("#").append(tc).append(" ").append(dp[s1][s2]).append("\n");
+			sb.append("#").append(tc).append(" ").append(dp[s1.length()][s2.length()]).append("\n");
 			
 		} // 전체 테스트 케이스 종료
 		System.out.println(sb);
